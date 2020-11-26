e102d493c03866d51d8dd3c9c7c8af7eb9204f7e
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-11-26 10:40:42 +0000

    Create the filterItems function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import items from './data';
+import Categories from './Categories';
+import Menu from './Menu';
 
 function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+  const filterItems = (category) => {
+    const newItems = items.filter((item) => item.catagory === category);
+    setMenuItems(newItems);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="menu-section">
+        <div className="title">
+          <h2>Our menu</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
   );
 }
 
